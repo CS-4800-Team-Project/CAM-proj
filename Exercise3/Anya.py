082279e1b32f42812a572af58085f229097ac015
adebelynska <123839070+adebelynska@users.noreply.github.com>
2023-02-21 22:35:54 -0800

    Create Anya.py

---
+++ Exercise3/Anya.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+print("Are there problems in your life? (y/n)")
+
+@app.route("/search/<yesorno>")
+def search_answer(yesorno):
+    if yesorno == 'y':
+        print("Can you solve it?")
+    elif yesorno == 'n':
+        print("Then don't worry!")
+    else:
+        print('Wrong input, please try again.')
+    return
+
+app.run(host = "0.0.0.0")
