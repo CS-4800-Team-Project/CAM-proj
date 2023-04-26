09aae7a57026eefe29c00790d96c195e3b1aa887
Michael Keshishian <89661528+Mkeshishian@users.noreply.github.com>
2023-04-26 13:01:22 -0700

    Fixed background and added search functions

--- Demo/backend.py
+++ Demo/backend.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, url_for, redirect
 import requests
-
+# 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
 app = Flask(__name__, template_folder="template")
 
 API_KEY = '8101db824a1c4a8ea99f3e7f77e01a75'
@@ -32,7 +32,22 @@ def search():
     else:
         return jsonify({'error': 'No search query provided'})
 
+@app.route('/recipe/<int:recipe_id>', methods=['GET'])
+def recipe_details(recipe_id):
+    recipe = get_recipe_details(recipe_id)
+    bg_image = url_for('static', filename='Food.jpg')
+    if recipe:
+        return render_template('recipe_details.html', recipe=recipe, bg_image=bg_image)
+    else:
+        return redirect(url_for('index'))
 
+def get_recipe_details(recipe_id):
+    url = f"https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={API_KEY}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
