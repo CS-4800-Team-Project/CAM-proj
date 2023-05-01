ea27a087b08dd342c4c59a2aacb7cff07f8aa05a
Michael Keshishian <89661528+Mkeshishian@users.noreply.github.com>
2023-05-01 00:35:49 -0700

    Added OpenAI for recipe generation

--- Demo/backend.py
+++ Demo/backend.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, jsonify, url_for, redirect
 import requests
-# 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
+import openai
+
+# Replace your_api_key with your actual OpenAI API key
+openai.api_key = "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"
+
 app = Flask(__name__, template_folder="template")
 
 API_KEY = '8101db824a1c4a8ea99f3e7f77e01a75'
@@ -13,12 +17,31 @@ def search_recipes(query):
     else:
         return []
 
+def generate_recipe(ingredients):
+    prompt = f"Create a delicious recipe using the following ingredients: {', '.join(ingredients)}.\n\nRecipe:"
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    recipe = response.choices[0].text.strip()
+    return recipe
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        search_query = request.form['search_query']
-        results = search_recipes(search_query)
-        return render_template('search_results.html', results=results)
+        if 'search_query' in request.form:
+            search_query = request.form['search_query']
+            results = search_recipes(search_query)
+            return render_template('search_results.html', results=results)
+        elif 'ingredients' in request.form:
+            ingredients = request.form['ingredients'].split(', ')
+            recipe = generate_recipe(ingredients)
+            return render_template('generated_recipe.html', recipe=recipe)
     return render_template('index.html')
 
 @app.route('/search', methods=['GET'])
@@ -51,4 +74,5 @@ def get_recipe_details(recipe_id):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
+
 
