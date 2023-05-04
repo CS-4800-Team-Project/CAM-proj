3abfb43d19cd9372321db391ae655d80fd3468c0
CJ(Catherine) Johnson <78944440+cgrjohnson@users.noreply.github.com>
2023-05-03 18:52:16 -0700

    Update backend.py

--- demo/backend.py
+++ demo/backend.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 # Set up database connection
 cnx = pymysql.connect(user='root', password='Blackops1871!',
                       host='127.0.0.1', database='recipes_db')
+# Enable user cursor
 cursor = cnx.cursor()
 
+# Execute search query to return recipe results
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -16,7 +18,9 @@ def index():
                        f"FROM recipes WHERE title LIKE '%{search_query}%'"
         cursor.execute(select_query)
         results = cursor.fetchall()
+        # Return value of search results to display file
         return render_template('results.html', results=results)
+    # Use result template from index display file
     return render_template('index.html')
 
 if __name__ == '__main__':
