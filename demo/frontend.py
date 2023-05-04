7dff60bac303e64b269bcee42bb5af68514ceddd
CJ(Catherine) Johnson <78944440+cgrjohnson@users.noreply.github.com>
2023-05-03 18:57:40 -0700

    Update frontend.py

--- demo/frontend.py
+++ demo/frontend.py
@@ -6,13 +6,16 @@ app = Flask(__name__)
 # Set up database connection
 cnx = pymysql.connect(user='root', password='Blackops1871!',
                       host='127.0.0.1', database='recipes_db')
+# Enable user cursor
 cursor = cnx.cursor()
 
 @app.route('/')
+# Define index function to use template for search results from display file
 def index():
     return render_template('index.html')
 
 @app.route('/search', methods=['POST'])
+# Define search function to find recipe info: title, URL, rating, reviews, num. of users who made recipe
 def search():
     query = request.form['query']
     select_query = f"SELECT title, url, rating, reviews, made_it_count " \
