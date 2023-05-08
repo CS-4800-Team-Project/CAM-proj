41de7a17a953d988665e67591fb55d23d20a90ef
Michael Keshishian <89661528+Mkeshishian@users.noreply.github.com>
2023-05-08 13:40:02 -0700

    Update backend.py

--- Demo/backend.py
+++ Demo/backend.py
@@ -3,18 +3,52 @@ import requests
 import openai
 import os
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-import pymysql
+import sqlite3
 from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
 import secrets
-from flask_login import login_required, current_user
-
 
 
 # Set up database connection
-db = pymysql.connect(user='root', password='Blackops1871!',
-                      host='127.0.0.1', database='recipes_db')
+db = sqlite3.connect('recipes_db.db', check_same_thread=False)
 cursor = db.cursor()
+
+
+def init_db():
+    # Create users table
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT UNIQUE NOT NULL,
+                        email TEXT UNIQUE NOT NULL,
+                        password TEXT NOT NULL)''')
+
+    # Create ratings table
+    cursor.execute('''CREATE TABLE IF NOT EXISTS ratings (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        user_id INTEGER NOT NULL,
+                        recipe_id INTEGER NOT NULL,
+                        rating INTEGER NOT NULL,
+                        FOREIGN KEY (user_id) REFERENCES users(id))''')
+
+    # Create favorites table
+    cursor.execute('''CREATE TABLE IF NOT EXISTS favorites (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        user_id INTEGER,
+                        recipe_id INTEGER,
+                        FOREIGN KEY (user_id) REFERENCES users(id))''')
+
+    # Create searches table
+    cursor.execute('''CREATE TABLE IF NOT EXISTS searches (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        user_id INTEGER,
+                        query TEXT,
+                        FOREIGN KEY (user_id) REFERENCES users(id))''')
+
+    # Commit the changes
+    db.commit()
+
+
+init_db()
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
@@ -64,6 +98,7 @@ def index():
             return render_template('generated_recipe.html', recipe=recipe, logo_image=logo_image, current_user=current_user)
     return render_template('index.html', logo_image=logo_image, current_user=current_user)
 
+
 @app.route('/search', methods=['GET'])
 def search():
     print('search function called')
@@ -78,7 +113,7 @@ def search():
 @app.route('/recipe/<int:recipe_id>', methods=['GET'])
 def recipe_details(recipe_id):
     recipe = get_recipe_details(recipe_id)
-    cursor.execute("SELECT COUNT(*) FROM favorites WHERE recipe_id = %s", (recipe_id,))
+    cursor.execute("SELECT COUNT(*) FROM favorites WHERE recipe_id = ?", (recipe_id,))
     favorites_count = cursor.fetchone()[0]
     bg_image = url_for('static', filename='Food.jpg')
     if recipe:
@@ -114,34 +149,13 @@ class User(UserMixin):
 
 @login_manager.user_loader
 def load_user(user_id):
-    cursor.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
     result = cursor.fetchone()
     if result:
         return User(result[0], result[1], result[2], result[3])
     else:
         return None
 
-def load_user_by_username(username):
-    connection = pymysql.connect(
-        host="127.0.0.1",
-        user="root",
-        password="Blackops1871!",
-        database="recipes_db"
-    )
-
-    cursor = connection.cursor()
-    query = "SELECT id, username, email, password FROM users WHERE username = %s"
-    cursor.execute(query, (username,))
-
-    result = cursor.fetchone()
-    connection.close()
-
-    if result:
-        user_id, username, email, password = result
-        user = User(user_id, username, email, password)
-        return user
-    else:
-        return None
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -162,27 +176,6 @@ def login():
     else:
         return render_template('login.html')
 
-def load_user_by_email(email):
-    connection = pymysql.connect(
-        host="127.0.0.1",
-        user="root",
-        password="Blackops1871!",
-        database="recipes_db"
-    )
-
-    cursor = connection.cursor()
-    query = "SELECT id, username, email, password FROM users WHERE email = %s"
-    cursor.execute(query, (email,))
-
-    result = cursor.fetchone()
-    connection.close()
-
-    if result:
-        user_id, username, email, password = result
-        user = User(user_id, username, email, password)
-        return user
-    else:
-        return None
 
 @app.route('/rate_recipe/<int:recipe_id>', methods=['POST'])
 @login_required
@@ -190,33 +183,17 @@ def rate_recipe(recipe_id):
     rating_value = request.form['rating']
     user_id = current_user.id
 
-    cursor.execute("SELECT * FROM ratings WHERE user_id = %s AND recipe_id = %s", (user_id, recipe_id))
+    cursor.execute("SELECT * FROM ratings WHERE user_id = ? AND recipe_id = ?", (user_id, recipe_id))
     existing_rating = cursor.fetchone()
 
     if existing_rating:
-        cursor.execute("UPDATE ratings SET rating = %s WHERE user_id = %s AND recipe_id = %s", (rating_value, user_id, recipe_id))
+        cursor.execute("UPDATE ratings SET rating = ? WHERE user_id = ? AND recipe_id = ?", (rating_value, user_id, recipe_id))
     else:
-        cursor.execute("INSERT INTO ratings (user_id, recipe_id, rating) VALUES (%s, %s, %s)", (user_id, recipe_id, rating_value))
+        cursor.execute("INSERT INTO ratings (user_id, recipe_id, rating) VALUES (?, ?, ?)", (user_id, recipe_id, rating_value))
 
     db.commit()
     return redirect(url_for('recipe_details', recipe_id=recipe_id))
 
-def check_username_exists(username):
-    connection = pymysql.connect(
-        host="127.0.0.1",
-        user="root",
-        password="Blackops1871!",
-        database="recipes_db"
-    )
-
-    cursor = connection.cursor()
-    query = "SELECT * FROM users WHERE username = %s"
-    cursor.execute(query, (username,))
-
-    result = cursor.fetchone()
-    connection.close()
-
-    return result is not None
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -232,19 +209,9 @@ def signup():
         # Save the new user to the database
         hashed_password = generate_password_hash(password, method='sha256')
 
-        connection = pymysql.connect(
-            host="127.0.0.1",
-            user="root",
-            password="Blackops1871!",
-            database="recipes_db"
-        )
-
-        cursor = connection.cursor()
-        query = "INSERT INTO users (username, password, email) VALUES (%s, %s, %s)"
+        query = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)"
         cursor.execute(query, (username, hashed_password, email))
-
-        connection.commit()
-        connection.close()
+        db.commit()
 
         return redirect(url_for('login'))
 
@@ -266,46 +233,12 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-
-def get_user_stats(user_id):
-    cursor.execute("SELECT recipe_id, rating FROM ratings WHERE user_id = %s ORDER BY rating DESC LIMIT 3", (user_id,))
-    top_ratings = cursor.fetchall()
-
-    cursor.execute("SELECT COUNT(*) FROM searches WHERE user_id = %s", (user_id,))
-    searches_count = cursor.fetchone()[0]
-
-    cursor.execute("SELECT recipe_id FROM favorites WHERE user_id = %s", (user_id,))
-    favorite_recipes = cursor.fetchall()
-
-    return top_ratings, favorite_recipes
-
-def toggle_favorite(user_id, recipe_id):
-    cursor.execute("SELECT * FROM favorites WHERE user_id = %s AND recipe_id = %s", (user_id, recipe_id))
-    existing_favorite = cursor.fetchone()
-
-    if existing_favorite:
-        cursor.execute("DELETE FROM favorites WHERE user_id = %s AND recipe_id = %s", (user_id, recipe_id))
-    else:
-        cursor.execute("INSERT INTO favorites (user_id, recipe_id) VALUES (%s, %s)", (user_id, recipe_id))
-
-    db.commit()
-
 @app.route('/toggle_favorite/<int:recipe_id>', methods=['POST'])
 @login_required
 def toggle_favorite_route(recipe_id):
     toggle_favorite(current_user.id, recipe_id)
     return redirect(url_for('recipe_details', recipe_id=recipe_id))
 
-@app.route('/favorite_recipe/<recipe_id>', methods=['POST'])
-def get_favorite_recipes(user_id):
-    cursor.execute("SELECT recipe_id FROM favorites WHERE user_id = %s", (user_id,))
-    favorite_recipe_ids = [row[0] for row in cursor.fetchall()]
-    favorite_recipes = []
-    for recipe_id in favorite_recipe_ids:
-        recipe = get_recipe_details(recipe_id)
-        if recipe:
-            favorite_recipes.append(recipe)
-    return favorite_recipes
 
 def get_profile_link():
     if current_user.is_authenticated:
@@ -313,6 +246,72 @@ def get_profile_link():
     else:
         return url_for('login')
 
+# Updated function
+def load_user_by_username(username):
+    cursor.execute("SELECT id, username, email, password FROM users WHERE username = ?", (username,))
+    result = cursor.fetchone()
+
+    if result:
+        user_id, username, email, password = result
+        user = User(user_id, username, email, password)
+        return user
+    else:
+        return None
+
+# Updated function
+def load_user_by_email(email):
+    cursor.execute("SELECT id, username, email, password FROM users WHERE email = ?", (email,))
+    result = cursor.fetchone()
+
+    if result:
+        user_id, username, email, password = result
+        user = User(user_id, username, email, password)
+        return user
+    else:
+        return None
+
+# Updated function
+def check_username_exists(username):
+    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+    result = cursor.fetchone()
+
+    return result is not None
+
+# Updated function
+def get_user_stats(user_id):
+    cursor.execute("SELECT recipe_id, rating FROM ratings WHERE user_id = ? ORDER BY rating DESC LIMIT 3", (user_id,))
+    top_ratings = cursor.fetchall()
+
+    cursor.execute("SELECT COUNT(*) FROM searches WHERE user_id = ?", (user_id,))
+    searches_count = cursor.fetchone()[0]
+
+    cursor.execute("SELECT recipe_id FROM favorites WHERE user_id = ?", (user_id,))
+    favorite_recipes = cursor.fetchall()
+
+    return top_ratings, favorite_recipes
+
+# Updated function
+def toggle_favorite(user_id, recipe_id):
+    cursor.execute("SELECT * FROM favorites WHERE user_id = ? AND recipe_id = ?", (user_id, recipe_id))
+    existing_favorite = cursor.fetchone()
+
+    if existing_favorite:
+        cursor.execute("DELETE FROM favorites WHERE user_id = ? AND recipe_id = ?", (user_id, recipe_id))
+    else:
+        cursor.execute("INSERT INTO favorites (user_id, recipe_id) VALUES (?, ?)", (user_id, recipe_id))
+
+    db.commit()
+
+# Updated function
+def get_favorite_recipes(user_id):
+    cursor.execute("SELECT recipe_id FROM favorites WHERE user_id = ?", (user_id,))
+    favorite_recipe_ids = [row[0] for row in cursor.fetchall()]
+    favorite_recipes = []
+    for recipe_id in favorite_recipe_ids:
+        recipe = get_recipe_details(recipe_id)
+        if recipe:
+            favorite_recipes.append(recipe)
+    return favorite_recipes
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
